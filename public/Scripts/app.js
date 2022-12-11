8caebb2592d37f2c89e1485ed1dff3f10fa34308
Yoaltzin Xolalpa <urano.balam@gmail.com>
2022-12-11 15:55:50 -0500

    delete app.

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -5,10 +5,3 @@
     window.addEventListener("load",Start);
 })();
 
-function ExportToExcel(type, fn, dl) {
-    var elt = document.getElementById('report_table');
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
-    return dl ?
-      XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }):
-      XLSX.writeFile(wb, fn || ('Report.' + (type || 'xlsx')));
-}
